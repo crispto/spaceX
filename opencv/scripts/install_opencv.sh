03f194bc6069b4d699518ae9873c472ea13de5c6
crispto <2273713147@qq.com>
2023-12-14 10:49:14 +0800

    add install ffmpeg

--- opencv/scripts/install_opencv.sh
+++ opencv/scripts/install_opencv.sh
@@ -1,11 +1,26 @@
+
+set -x
+set +e
+sudo apt-get -y install libgtk2.0-dev
+
+
+set -e
 opencv_url="https://github.com/opencv/opencv/archive/refs/tags/4.8.1.zip"
 opencv_contrib_url="https://github.com/opencv/opencv_contrib/archive/refs/tags/4.8.1.zip"
 
+cwd=$(pwd)
+tgt=$1/opencv_install
+if [ -d "$tgt" ]; then
+    echo "removing $tgt"
+    rm -rf $tgt;
+fi
+mkdir -p $tgt
+cd $tgt
 wget --progress=bar:force $opencv_url -O opencv.zip
 wget --progress=bar:force $opencv_contrib_url -O opencv_contrib.zip
 
 unzip opencv.zip
 unzip opencv_contrib.zip
 cd opencv-4.8.1
-
-sudo apt-get -y install  build-essential checkinstall cmake pkg-config yasm  libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev  libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils libtiff5-dev libjpeg62-dev ffmpeg libatlas-base-dev gfortran
+ln -s $cwd/Makefile .
+make build
--- scripts/perf.sh
+++ scripts/perf.sh
@@ -4,7 +4,7 @@
 # help function
 function usage(){
     echo "Usage: $0 [install|test|clean|run|frame]"
-    
+
 }
 
 # install frame graph tools, and install perf if not installed
@@ -20,7 +20,7 @@ function install(){
     sudo update-alternatives --install /usr/bin/stackcollapse-perf.pl stackcollapse `pwd`/stackcollapse-perf.pl 60
     sudo update-alternatives --install /usr/bin/flamegraph.pl framegraph `pwd`/flamegraph.pl 60
     cd .. && rm -rf FlameGraph
-    
+
     #run rest
     if ! perf_test ; then
         echo "perf test failed"
@@ -30,8 +30,8 @@ function install(){
 
 # test perf command on sleep 10
 function perf_test(){
-    #perf record -F 99 -a -g -- sleep 10
-    perf record --call-graph dwarf -a sleep 2
+    perf record -F 99 -a -g -- sleep 10
+    # perf record --call-graph dwarf -a sleep 2
     sudo chmod -R 777 perf.data
     perf script -i perf.data > out.perf
     stackcollapse-perf.pl out.perf > out.folded
@@ -46,7 +46,7 @@ function clear(){
     # clear this dir, if not exist, dont care
     rm -rf *.perf
     rm -rf *.data
-    rm -rf  *.svg 
+    rm -rf  *.svg
     rm -rf *.data.old
     rm -rf *.folded
 }
